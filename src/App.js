eff5e7ad06376e44f83d85c0abc7307213dc4fb1
Edouardgj <edouard.grandjean@hotmail.fr>
2022-06-21 09:06:08 +0200

    import switch route browser

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 // eslint-disable-next-line
 import logo from "./img/logo.svg";
 // eslint-disable-next-line
 import "./App.css";
 import home from "./pages/Home";
-import faq from './pages/faq';
-import NotFound from './pages/NotFound';
+import faq from "./pages/faq";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-   <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={home} /> 
-      <Route path="/faq" exact component={faq} /> 
-      <Route component={NotFound} />
-      <Route path="faq" exact component={faq} />
-    </Switch>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={home} />
+        <Route path="/faq" exact component={faq} />
+        <Route component={NotFound} />
+        <Route path="faq" exact component={faq} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
