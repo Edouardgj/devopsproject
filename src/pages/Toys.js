e8dee9ab7bbb7133d1fb47c19ae04e1d22cec05d
Edouardgj <edouard.grandjean@hotmail.fr>
2022-06-22 22:39:25 +0200

    recherche&json-server

--- src/pages/Toys.js
+++ src/pages/Toys.js
@@ -1,32 +1,46 @@
 import React, {useState, useEffect} from 'react'
 
+
+
 const Toys =() => {
-    const [Toys, setToys ] = useState([
-      
-    ]);
-    useEffect(() => {
-      fetch("http://localhost:4000/toyslist")
-        .then((res) => res.json())
-        .then((Toys) => {
-          setToys(Toys);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }, []);
-    return (
-      <div className='divjouets'> 
-      <ul>
-        {Toys.map((Toys) => (
-          <div>
-          <li key={Toys.id}><h4> {Toys.name} </h4></li>
+  const [Toys, setToys ] = useState([
+    
+  ]);
+  const [query, setQuery] = useState("")
+  useEffect(() => {
+    fetch("http://localhost:4000/toyslist")
+      .then((res) => res.json())
+      .then((Toys) => {
+        setToys(Toys);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <>
+    <div className='divjouets'> 
+    <input placeholder="Votre jouet" onChange={event => setQuery(event.target.value)}/>
+    <ul>
+    {Toys.filter(Toys => {
+    if (query === '') {
+      return Toys;
+    } else if (Toys.name.toLowerCase().includes(query.toLowerCase())) {
+      return Toys;
+    }
+  }).map((Toys) => (
+        <div>
+          <li> <h1> {Toys.name} </h1> </li>
           <li> <h4> {Toys.price} </h4> </li>
-          </div>
-        ))}
-      </ul>
-      </div>
-    );
-  };
-  
-  
-  export default Toys
+          <li> <p>{Toys.description} </p> </li>
+        </div>
+      ))}
+    </ul>
+    </div>
+
+    </>
+  );
+};  
+
+
+export default Toys
