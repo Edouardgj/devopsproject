5a8c24e69029b0aa8bb419785b32cd8101c0e036
Edouardgj <edouard.grandjean@hotmail.fr>
2022-06-22 10:49:34 +0200

    call db

--- src/pages/Toys.js
+++ src/pages/Toys.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
 
-
-const Toys = () => {
+const Toys =() => {
+    const [Toys, setToys ] = useState([
+      
+    ]);
+    useEffect(() => {
+      fetch("http://localhost:4000/toyslist")
+        .then((res) => res.json())
+        .then((Toys) => {
+          setToys(Toys);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
     return (
-      <div>
-            <div className="titre1">
-                <h1>BDécouvrez tous nos jouets</h1>
-                <div className="subtitle1">
-                    <h2>Amusez vos enfants à prix mini</h2>
-                </div>
-            </div>
-        </div>
+      <div className='divjouets'> 
+      <ul>
+        {Toys.map((Toys) => (
+          <div>
+          <li key={Toys.id}><h4> {Toys.name} </h4></li>
+          <li> <h4> {Toys.price} </h4> </li>
+          </div>
+        ))}
+      </ul>
+      </div>
     );
-  }
+  };
+  
   
   export default Toys
